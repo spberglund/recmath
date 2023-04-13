e576e83c86440107f8a748f52c4c7edd609ea44f
Spencer Berglund <spberglund@gmail.com>
2023-04-13 00:53:33 -0700

    More patterns...

--- spirals/spirals.py
+++ spirals/spirals.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import colorsys
 
 def on_segment(p, q, r):
     return (q[0] <= max(p[0], r[0])) and (q[0] >= min(p[0], r[0])) and (q[1] <= max(p[1], r[1])) and (q[1] >= min(p[1], r[1]))
@@ -42,13 +43,20 @@ def check_intersection(points, new_point):
     return False
 
 
-def draw_line_segments(line_segments, ax):
-    for i in range(len(line_segments) - 1):
-        p1, p2 = line_segments[i], line_segments[i + 1]
-        ax.plot([p1[0], p2[0]], [p1[1], p2[1]], marker='.', color='blue')
+def draw_line_segments(points, ax, title):
 
-    # Set equal aspect ratio for X and Y axes
+    if points:
+        for i in range(len(points) - 1):
+            p1, p2 = points[i], points[i + 1]
+
+            clr = tuple(int(c * 255) for c in colorsys.hls_to_rgb(1.0 - (i/len(points)), 0.5, 0.8))
+            ax.plot([p1[0], p2[0]], [p1[1], p2[1]], marker='', color=f'#{clr[0]:02x}{clr[1]:02x}{clr[2]:02x}')
+
+    ax.set_title(title)
+    ax.set_axis_off()
     ax.set_aspect('equal', adjustable='box')
+
+
 
 
 
@@ -101,30 +109,101 @@ def add_most_clockwise_point(points, rise_run):
             points.append(candidate_point)
             return True
 
-    print("col")
     return False
 
-def plot_sequence(seq, ax, maxiter=1000):
+def exec_sequence(seq, maxiter=500):
     points = []
-    ax.set_axis_off()
     for i in range(maxiter):
         if not add_most_clockwise_point(points, seq[i % len(seq)]):
             break
-    
-    draw_line_segments(points, ax)
 
-    numsegs = len(points)-1
+    return points
 
-    ax.set_title(f"{' → '.join(f'{a}x{b}' for a, b in seq)}{'=' if numsegs < maxiter else '≥'}{numsegs}")
+
+def iter_riseruns(n):
+
+    rr = [0,0]
+
+    i = 0
+
+    while n is None or i < n:
+        i += 1
+        if rr[0] >= rr[1]:
+            rr[0] = 0
+            rr[1] += 1
+        else:
+            rr[0] += 1
+
+        yield tuple(rr)
+
+def is_base_seq(seq):
+    gcds = [np.gcd(a, b) for a, b in seq]
+    gcdall = gcds[0]
+
+    for n in gcds[1:]:
+        gcdall = np.gcd(gcdall, n)
+
+    return gcdall == 1
+
+def iter_seqs(dims):
+    rr_iters = [iter_riseruns(None) for i in range(dims)]
+    cur_seq = [(0,1)]*dims
+
+    d = 0
+
+    while True:
+        cur_seq[d] = next(rr_iters[d])
+
+        if is_base_seq(cur_seq):
+            yield tuple(cur_seq)
+
+        d += 1
+        if d >= dims:
+            d = 0
+
 
 if __name__ == "__main__":
 
-    dimx, dimy = 8, 8
+    maxiter = 500
 
+    #Draw one
+    # seq = ((1,2), (2,3))
+
+    # points = exec_sequence(seq, maxiter)
+
+    # numsegs = len(points)-1
+    # title = f"{' → '.join(f'{a}x{b}' for a, b in seq)}{'=' if numsegs < maxiter else '≥'}{numsegs}"
+
+    # print(title)
+
+    # fig, ax = plt.subplots()
+    # draw_line_segments(points, ax, title)
+    # plt.show()
+    
+    # Draw multiple
+    dimx, dimy = 10, 10
     fig, ax = plt.subplots(dimx, dimy)
 
-    for x in range(dimx):
-        for y in range(dimy):
-            plot_sequence([(1,x),(1,y)], ax[x, y])
+    for i, seq in enumerate(iter_seqs(2)):
+
+        if i >= dimx * dimy:
+            break
+
+        x, y = i // dimy, i % dimx
+
+        print(seq)
+        if not is_base_seq(seq):
+            print("DUPLICATE")
+            draw_line_segments(None, ax[x, y], None)
+            continue
+
+        points = exec_sequence(seq, maxiter)
+
+        numsegs = len(points)-1
+        title = f"{' → '.join(f'{a}x{b}' for a, b in seq)}{'=' if numsegs < maxiter else '≥'}{numsegs}"
+
+        print(title)
+
+        draw_line_segments(points, ax[x, y], title)
 
     plt.show()
